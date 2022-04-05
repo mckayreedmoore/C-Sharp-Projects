0973bbb6c59e3f3a96fbb00376f68e5fb945765c
mckayreedmoore <75471605+mckayreedmoore@users.noreply.github.com>
2022-04-05 12:41:40 -0600

    Updated Homework

--- Basic_C#_Programs/1NEW/TwentyOneCardGame/Program.cs
+++ Basic_C#_Programs/1NEW/TwentyOneCardGame/Program.cs
@@ -17,9 +17,21 @@ namespace TwentyOneCardGame
             
             Console.WriteLine("Welcome to the Grand Hotel and Casino! \nWhat is your name?");
             string playerName = Console.ReadLine();
-            Console.WriteLine("How much money did you bring today?");
-            int bank = Convert.ToInt32(Console.ReadLine());
+            
+
+            bool validAnswer = false;
+            int bank = 0;
+            while (!validAnswer)
+            {
+                Console.WriteLine("And How much money did you bring today?");
+                validAnswer = int.TryParse(Console.ReadLine(), out bank);
+                if (!validAnswer) Console.WriteLine("Please enter digits only, no decimals");
+            }
+
+            
             Console.WriteLine("Hello, {0}. Would you like to join a game of Twenty-One?", playerName);
+
+
             string answer = Console.ReadLine().ToLower();
             if (answer == "yes" || answer == "yea" || answer == "y" || answer == "ya")
             {
@@ -35,7 +47,23 @@ namespace TwentyOneCardGame
                 player.isActivelyPlaying = true;
                 while (player.isActivelyPlaying && player.Balance > 0)
                 {
-                    game.Play();
+                    try
+                    {
+                        game.Play();
+                    }
+                    catch(FraudException)
+                    {
+                        Console.WriteLine("Security! Kick this person out! They are trying to cheat the system!");
+                        Console.ReadLine();
+                        return;
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("An error occurred. Please contact your system administrator.");
+                        Console.ReadLine();
+                        return;
+                    }
+                    
                 }
                 //game -= player;
                 Console.WriteLine("Thank you for playing!");
---
+++ Basic_C#_Programs/TryCatchAssignment13/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchAssignment13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Ask user for their age. Cast their input to an integer. If the age is less than 0, throw an exception. Take their age and subtract it from the current year. Guess their birthyear. 
+                Console.WriteLine("What will be your age this year?");
+                int age = Convert.ToInt32(Console.ReadLine());
+
+                if (age < 0)
+                {
+                    throw new Exception();
+                }
+                int birthYear = 2022 - age;
+                Console.WriteLine("Let me guess... \nYou were born in {0}", birthYear);
+                Console.ReadLine();
+            }
+            
+            catch (Exception)
+            {
+                Console.WriteLine("Type only Numbers. You can't have 0 age or negative numbers either!");
+            }
+
+
+            
+        }
+    }
+}
