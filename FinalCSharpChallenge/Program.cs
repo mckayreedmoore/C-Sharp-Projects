d44ead4d21c895ec1115b74d3f20e6ed81cc9e4a
mckayreedmoore <75471605+mckayreedmoore@users.noreply.github.com>
2022-04-14 15:17:53 -0600

    Updated Homework

---
+++ FinalCSharpChallenge/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalCSharpChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var aStudent = new Student() { firstName = "Jim", lastName = "Bo", isFulltimeStudent = true };
+
+                ctx.Students.Add(aStudent);
+                ctx.SaveChanges();
+
+            }
+        }
+    }
+
+    public  class SchoolContext: DbContext
+    {
+        public SchoolContext(): base()
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+    public class Student
+    {
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public bool isFulltimeStudent { get; set; }
+        
+    }
+}
